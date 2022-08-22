9050afce94e8130fb89f0b05c1a8a90cd58d7900
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-08-22 12:05:29 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * 19. const、let等の変数宣言
+ */
+
+//  var val1 = "var変数";
+//  console.log(val1);
+
+//  //var変数は上書き可能
+//  val1 = "var変数は上書き可能";
+//  console.log(val1);
+
+//  //var変数は再宣言可能
+//  var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = "let変数は上書き可能";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数は再宣言不可能"；
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // constは上書き不可
+// // val3 = "const変数は上書き不可"
+
+// // constは再宣言不可
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "ビオレ",
+//   age: 40,
+// };
+// val4.name = "Biore";
+// val4.adress = "meguro";
+// console.log(val4);
