d056842b9ea4adedb1910c1d5df18d428fe3c685
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-08-22 17:21:53 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -40,3 +40,45 @@
 // val4.name = "Biore";
 // val4.adress = "meguro";
 // console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ['dog','cat'];
+// val5[0] = 'bird'
+// val5.push ("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "加藤";
+// const age = "51";
+// //名前は加藤です。年齢は51歳です。
+// const message1 = "名前は" + name + "です。年齢は"　+ age +"歳です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いる方法
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str){
+//   return str;
+// };
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("ファンクション1です。"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("ファンクション2です。"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 30));
