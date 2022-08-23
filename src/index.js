845b276f51409094592491bdf3513d34914bb286
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-08-23 16:35:00 +0900

    分割代入途中

--- src/index.js
+++ src/index.js
@@ -67,18 +67,32 @@
 // function func1(str){
 //   return str;
 // };
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("ファンクション1です。"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("ファンクション1です。"));
 
-//アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("ファンクション2です。"));
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("ファンクション2です。"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 30));
+
+/**
+ * 分割代入
+ */
+const myProfile = {
+  name: "加藤",
+  age: 51
 };
-console.log(func3(10, 30));
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}です。`;
+console.log(message2);
